ff7666b729d1af4b7e6fc16dc47ba8c700266cf1
pharm-ack <olumofe6@gmail.com>
2024-07-06 07:13:46 -0700

    second commit

--- src/components/collections.tsx
+++ src/components/collections.tsx
@@ -18,7 +18,7 @@ export default function Collections() {
     },
   ];
   return (
-    <div className="bg-white py-6 sm:py-8 lg:py-12">
+    <div id="#collection" className="bg-white py-6 sm:py-8 lg:py-12">
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="mb-8 flex flex-col items-center">
           <h2 className="mb-2 text-center text-2xl font-bold text-gray-800 lg:text-3xl">
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,7 @@
 // import heroImg from "../hero.jpg"
 
+import Link from "next/link";
+
 export default function Hero() {
   return (
     <div
@@ -9,11 +11,18 @@ export default function Hero() {
       <div className="flex h-full w-full items-center justify-center bg-gray-900/40">
         <div className="text-center">
           <h1 className="text-3xl font-semibold text-white lg:text-4xl">
-            Build your new <span className="text-blue-400">Saas</span> Project
+            Unleash Your Inner Style Icon
           </h1>
-          <button className="mt-4 w-full transform rounded-md bg-blue-600 px-5 py-2 text-sm font-medium capitalize text-white transition-colors duration-300 hover:bg-blue-500 focus:bg-blue-500 focus:outline-none lg:w-auto">
-            Start project
-          </button>
+          <p className="mt-4 text-sm text-white lg:text-base">
+            Discover the latest trends in men’s fashion, tailored for every
+            occasion.
+          </p>
+          <Link
+            href="#collection"
+            className="mt-4 w-full transform rounded-md bg-yellow-400 px-5 py-2 text-sm font-medium capitalize text-white transition-colors duration-300 hover:bg-blue-500 focus:bg-blue-500 focus:outline-none lg:w-auto"
+          >
+            View Our Collection
+          </Link>
         </div>
       </div>
     </div>

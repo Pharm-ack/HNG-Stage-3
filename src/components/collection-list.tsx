ce0c692e9c3dafeda1b9d4ebf9b93fade3f6b742
Pharm-ack <75676193+Pharm-ack@users.noreply.github.com>
2024-07-11 15:24:50 -0700

    Update collection-list.tsx

--- src/components/collection-list.tsx
+++ src/components/collection-list.tsx
@@ -41,6 +41,13 @@ export default function CollectionList({ product }: productProps) {
         <p className="text-default-500 text-cneter text-sm font-medium">
           {formatPrice(product.price)}
         </p>
+        <div className="flex items-center gap-1">
+          {[...Array(4)].map((_, i) => (
+            <Image key={i} src="/star.png" width={20} height={20} alt="star" />
+          ))}
+          <Image src="/blackstart.png" width={19} height={19} alt="star" />{" "}
+          <span className="text-sm">(4.7)</span>
+        </div>
       </div>
 
       {/* <button className="absolute right-[5%] top-[30%] -translate-x-1/2 translate-y-1/2 rounded-full bg-yellow-500 p-2 px-3 text-white opacity-0 hover:bg-yellow-600">

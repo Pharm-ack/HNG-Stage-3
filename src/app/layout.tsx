06112f75cdf3ec1b67c888e09916487a7a69a9d6
Pharm-ack <75676193+Pharm-ack@users.noreply.github.com>
2024-07-12 08:34:21 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className}flex flex-col min-h-[100vh]`}>
+      <body className={`${inter.className}flex flex-col min-h-screen`}>
         <Announcement />
         <CartProvider>
           <Header />
